2558200a5687ac09cea517c3be5ee7864bacf5f5
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2016-08-08 15:50:45 -0400

    changes in price calculation formula

--- app/models/spree/calculator_cruise.rb
+++ app/models/spree/calculator_cruise.rb
@@ -2,18 +2,17 @@ module Spree
   class CalculatorCruise < BaseCalculator
 
      def calculate_price(context, product, variant, options)
-       return [price:product.price.to_f] if product.rates.empty?
-      list = product.variants.where(id: variant.id).first.rates
-      array = []
-      list.each do |r|
-         if Date.parse(r.start_date) <=  Date.parse(context.start_date(options).to_s) &&
+        return [price:product.price.to_f] if product.rates.empty?
+        list = product.variants.where(id: variant.id).first.rates
+        array = []
+        list.each do |r|
+           if Date.parse(r.start_date) <=  Date.parse(context.start_date(options).to_s) &&
               Date.parse(r.end_date) >=  Date.parse(context.start_date(options).to_s)
-            double = r.send('double').to_f
-            adult_count = context.adult(options).to_i
-            price = adult_count/2 * double + adult_count%2 * double
-            array << {price: price, rate: r.id, avg: double}
+              adult_count = context.adult(options).to_i
+              price = adult_count/2 * r.double + adult_count%2 * r.double
+              array << {price: price, rate: r.id, avg: double}
+           end
         end
-      end
       array
     end
 
